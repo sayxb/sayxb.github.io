baaf5451af210e8796ee3ea42a6a6d5eab1d6a9c
sayxb <69209528+sayxb@users.noreply.github.com>
2026-09-01 23:43:55 +0000

    Deploying to gh-pages from @ sayxb/sayxb.github.io@151c355d7628cb3de6224028a333755423580f3e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,9 +16,9 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/blog/";
           },
-        },{id: "nav-projects",
-          title: "projects",
-          description: "A growing collection of your cool projects.",
+        },{id: "nav-",
+          title: "",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
