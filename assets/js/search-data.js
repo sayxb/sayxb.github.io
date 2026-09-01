9873e43d433d36440a8257f14c41b6516a706ea0
sayxb <69209528+sayxb@users.noreply.github.com>
2026-09-01 15:12:08 +0000

    Deploying to gh-pages from @ sayxb/sayxb.github.io@2c2439b825f3a983b3d33739f987c39cd5d5db06 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -32,7 +32,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "CV",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "You can download my CV here, upto date as of 01/09/26",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
