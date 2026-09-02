1ad57418a34f1ae901099affbf5340f3839078cb
sayxb <69209528+sayxb@users.noreply.github.com>
2026-09-02 13:52:42 +0000

    Deploying to gh-pages from @ sayxb/sayxb.github.io@5aaaad733b0e0bd1b1f143aee1e2dac764392414 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -414,6 +414,11 @@ ninja.data = [{
           description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
+            },},{id: "projects-masco-stock-pitch-sem1-25-26",
+          title: 'MASCO stock pitch SEM1 25/26',
+          description: "",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/masco_sem1/";
             },},{id: "teachings-data-science-fundamentals",
           title: 'Data Science Fundamentals',
           description: "This course covers the foundational aspects of data science, including data collection, cleaning, analysis, and visualization. Students will learn practical skills for working with real-world datasets.",
