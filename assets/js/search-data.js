07da8870762c5feed3e93b899499fab40f4c06b4
sayxb <69209528+sayxb@users.noreply.github.com>
2026-09-02 00:36:37 +0000

    Deploying to gh-pages from @ sayxb/sayxb.github.io@127a4db4514bba5497acbd6af7bdee52d46ee8fd 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -409,8 +409,8 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-aecom-stock-pitch",
-          title: 'AECOM stock pitch',
+          section: "News",},{id: "projects-aecom-stock-pitch-sem2",
+          title: 'AECOM stock pitch SEM2',
           description: "with background image",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
