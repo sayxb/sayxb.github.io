7e48ddd6577f9f9684a845ece3b0ef6171aa686c
sayxb <69209528+sayxb@users.noreply.github.com>
2026-09-07 02:22:28 +0000

    Deploying to gh-pages from @ sayxb/sayxb.github.io@9f0671aac2469742aaad56bd9f0a3d3e1845ba4a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -409,7 +409,7 @@ ninja.data = [{
               window.location.href = "/projects/1_project/";
             },},{id: "projects-homology-of-the-building-of-f",
           title: 'Homology of the Building of F₂³',
-          description: "Constructing the Tits Building of $\mathbb{F}_2^3$, computing its homology, and recovering the dimension of $St_3(\mathbb{F}_2)$. Picture shown is the Heawood graph, which is equivalent to the Building on $\mathbb{F}_2^3$",
+          description: "Constructing the Tits Building of $\mathbb{F}_2^3$, computing its homology, and recovering the dimension of $\mathrm{St}_3(\mathbb{F}_2)$. Picture shown is the Heawood graph, which is equivalent to the Building on $\mathbb{F}_2^3$",
           section: "Projects",handler: () => {
               window.location.href = "/projects/building-F_2%5E3-writeup/";
             },},{id: "projects-masco-stock-pitch-sem1-25-26",
